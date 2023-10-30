f2f89c82932aeb1a7beb9433069c88a29fb9f451
Coby Cho <cmcho@CougarNet.UH.EDU>
2023-10-29 21:04:58 -0500

    Added index code for ticket sales

--- ZooWeb/Pages/Index.cshtml.cs
+++ ZooWeb/Pages/Index.cshtml.cs
@@ -1,20 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 
-namespace ZooWeb.Pages
+namespace ZooWeb.Pages.TicketSales
 {
-    public class IndexModel : PageModel
-    {
-        private readonly ILogger<IndexModel> _logger;
+	public class IndexModel : PageModel
+	{
+		public List<TicketSaleInfo> ListTicketSales = new List<TicketSaleInfo>();
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+		public void OnGet()
+		{
+			string connectionString = "Server=tcp:yourdbserver.database.windows.net,1433;Database=YourDatabase;User ID=youruser;Password=yourpassword;Trusted_Connection=False;Encrypt=True;";
 
-        public void OnGet()
-        {
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				connection.Open();
+				String sql = "SELECT * FROM TicketSales";
+				using (SqlCommand command = new SqlCommand(sql, connection))
+				{
+					using (SqlDataReader reader = command.ExecuteReader())
+					{
+						while (reader.Read())
+						{
+							TicketSaleInfo info = new TicketSaleInfo();
+							info.ReceiptNumber = reader.GetInt32(0).ToString();
+							info.Date = reader.GetDateTime(1).ToString("yyyy-MM-dd");
+							info.Total = reader.GetDecimal(2).ToString();
+							info.SalesTax = reader.GetDecimal(3).ToString();
+							info.TicketID = reader.GetInt32(4).ToString();
+							info.PassType = reader.GetString(5);
+							info.Essn = reader.GetInt32(6).ToString();
 
-        }
-    }
+							ListTicketSales.Add(info);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	public class TicketSaleInfo
+	{
+		public string ReceiptNumber;
+		public string Date;
+		public string Total;
+		public string SalesTax;
+		public string TicketID;
+		public string PassType;
+		public string Essn;
+	}
 }
