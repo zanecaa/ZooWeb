e51a8bb15d017e073e1d2394fdb7527ebca1121f
Pouya <Pouyaraj74@gmail.com>
2023-11-01 10:11:18 -0500

    Shared layout was updated. Amenity Sales and Department have been added.

---
+++ ZooWeb/Pages/AmenitySales/Index.cshtml.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+using System.Numerics;
+
+
+namespace ZooWeb.Pages.AmenitySales
+{
+    public class IndexModel : PageModel
+    {
+        public List<AmenitytSalesInfo> ListAmentySales = new List<AmenitytSalesInfo>();
+
+        public void OnGet()
+        {
+
+            string connectionString = "Server=tcp:zoowebdbserver.database.windows.net,1433;Database=ZooWeb_db;User ID=zooadmin;Password=peanuts420!;Trusted_Connection=False;Encrypt=True;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                String sql = "SELECT * FROM amenitySales";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            AmenitytSalesInfo info = new AmenitytSalesInfo();
+                            info.EID = reader.GetInt32(0).ToString();
+                            info.LocationID = reader.GetInt32(1).ToString();
+                            info.SaleType = reader.GetString(2);
+                            info.SaleDate = reader.GetDateTime(3).ToString("yyyy-MM-dd");
+							info.Total = reader.GetDecimal(4).ToString();
+                            info.ReceiptNumber = reader.GetInt64(5).ToString();
+
+                            ListAmentySales.Add(info);
+                        }
+                    }
+                }
+            }
+        }
+    }
+        
+}
+public class AmenitytSalesInfo
+{
+    public string EID;
+    public string LocationID;
+    public string SaleType;
+    public string SaleDate;
+    public string Total;
+    public string ReceiptNumber;
+}
---
+++ ZooWeb/Pages/Department/Index.cshtml.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+using System.Numerics;
+
+
+namespace ZooWeb.Pages.Department
+{
+	public class IndexModel : PageModel
+	{
+		public List<DepartmentInfo> ListDepartment = new List<DepartmentInfo>();
+
+		public void OnGet()
+		{
+
+			string connectionString = "Server=tcp:zoowebdbserver.database.windows.net,1433;Database=ZooWeb_db;User ID=zooadmin;Password=peanuts420!;Trusted_Connection=False;Encrypt=True;";
+
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				connection.Open();
+				String sql = "SELECT * FROM department";
+				using (SqlCommand command = new SqlCommand(sql, connection))
+				{
+					using (SqlDataReader reader = command.ExecuteReader())
+					{
+						while (reader.Read())
+						{
+							DepartmentInfo info = new DepartmentInfo();
+							info.Dnumber = reader.GetInt16(0).ToString();
+							info.Name = reader.GetString(1);
+
+							ListDepartment.Add(info);
+						}
+					}
+				}
+			}
+		}
+	}
+
+}
+public class DepartmentInfo
+{
+	public string Dnumber;
+	public string Name;
+}
